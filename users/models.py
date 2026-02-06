5e3efb88355e3e88a5bb38edfc356054153569cb
PC <sergei1606@gmail.com>
2026-02-06 06:57:05 +0200

    feat: реализация личного кабинета (ТЗ 3.5)
    - Созданы модели UserProfile и DeliveryAddress
    - Реализована регистрация пользователей (отдельно от админки)
    - Добавлены формы: регистрация, вход, обновление профиля, смена пароля
    - Созданы views для всех операций личного кабинета
    - Реализованы шаблоны: регистрация, вход, профиль, история заказов, адреса
    - Настроены сигналы для автоматического создания профилей
    - Добавлена навигация в шапке сайта
    - Работает session-based аутентификация

--- users/models.py
+++ users/models.py
@@ -1,3 +1,67 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+
+class UserProfile(models.Model):
+    """Расширение стандартной модели User"""
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    phone = models.CharField(max_length=20, blank=True, verbose_name='Телефон')
+    avatar = models.ImageField(upload_to='avatars/', blank=True, null=True, verbose_name='Аватар')
+    birth_date = models.DateField(blank=True, null=True, verbose_name='Дата рождения')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Создан')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Обновлен')
+
+    class Meta:
+        verbose_name = 'Профиль пользователя'
+        verbose_name_plural = 'Профили пользователей'
+
+    def __str__(self):
+        return f'Профиль {self.user.username}'
+
+
+class DeliveryAddress(models.Model):
+    """Адреса доставки пользователя"""
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='addresses')
+    title = models.CharField(max_length=100, verbose_name='Название адреса', default='Дом')
+    full_name = models.CharField(max_length=200, verbose_name='ФИО')
+    phone = models.CharField(max_length=20, verbose_name='Телефон')
+    country = models.CharField(max_length=100, verbose_name='Страна', default='Россия')
+    city = models.CharField(max_length=100, verbose_name='Город')
+    street = models.CharField(max_length=200, verbose_name='Улица')
+    house = models.CharField(max_length=20, verbose_name='Дом')
+    apartment = models.CharField(max_length=20, blank=True, verbose_name='Квартира')
+    postal_code = models.CharField(max_length=20, verbose_name='Индекс')
+    is_primary = models.BooleanField(default=False, verbose_name='Основной адрес')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Создан')
+
+    class Meta:
+        verbose_name = 'Адрес доставки'
+        verbose_name_plural = 'Адреса доставки'
+        ordering = ['-is_primary', '-created_at']
+
+    def __str__(self):
+        return f'{self.title} - {self.city}, {self.street}, {self.house}'
+
+    def save(self, *args, **kwargs):
+        # Если этот адрес становится основным, снимаем флаг с других адресов пользователя
+        if self.is_primary:
+            DeliveryAddress.objects.filter(user=self.user, is_primary=True).update(is_primary=False)
+        super().save(*args, **kwargs)
+
+
+# Сигналы для автоматического создания профиля при создании пользователя
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    if hasattr(instance, 'profile'):
+        instance.profile.save()
