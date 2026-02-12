6f7f1e133e34e4a54d1fc88897af18fb877a4531
PC <sergei1606@gmail.com>
2026-02-12 13:24:35 +0200

    🎯 Final: working version with PostgreSQL and superuser

--- users/apps.py
+++ users/apps.py
@@ -6,4 +6,4 @@ class UsersConfig(AppConfig):
     name = 'users'
 
     def ready(self):
-        import users.signals  # Импортируем сигналы
+        import users.signals  # Импортируем сигналы    
